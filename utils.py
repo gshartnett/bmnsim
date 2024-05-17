1eeb5222f00628cf9ce5f6a3a0d39a274898b41b
Gavin Hartnett <gshartnett@gmail.com>
2024-05-16 17:46:50 -0700

    begin update to make code Qiskit==1.0 compatible

--- utils.py
+++ utils.py
@@ -1,4 +1,101 @@
 import numpy as np
+import sympy as sp
+
+
+number_of_matrices = 3
+gauge_group_degree = 2 # the N in SU(N)
+number_of_generators = gauge_group_degree**2 - 1 #understand the diff b/w U(N) and SU(N)
+
+bits_per_oscillator = 2
+number_states_per_oscillator = 2**bits_per_oscillator
+
+number_qubits = (
+    number_of_matrices
+    * number_of_generators
+    * bits_per_oscillator
+)
+
+print(f'number_qubits {number_qubits}, Hilbert space dimension {2**number_qubits}')
+index_map = {}
+counter = 0
+for i in range(number_of_matrices):
+    for a in range(number_of_generators):
+        index_map[(i,a)] = counter
+        counter += bits_per_oscillator
+index_map_inverse = dict(map(reversed, index_map.items()))
+
+
+# Eq. 4.5 of https://arxiv.org/pdf/2011.06573
+single_qubit_state_map = {
+    '00':0.5*(sp.symbols("I") + sp.symbols('Z')),
+    '01':0.5*(sp.symbols("X") + 1j*sp.symbols('Y')),
+    '10':0.5*(sp.symbols("X") - 1j*sp.symbols('Y')),
+    '11':0.5*(sp.symbols("I") - sp.symbols('Z'))
+    }
+
+
+def annihilation_operator_old(matrix_idx: int, generator_idx: int) -> sp.core.add.Add:
+    """
+    Build the annihilation operator for the oscillator.
+
+    Parameters
+    ----------
+    matrix_idx : int
+        The SO(3) index enumerating the matrices (1, 2, 3)
+    generator_idx : int
+        The generator index (1, 2, ..., N^2)
+
+    Returns
+    -------
+    sp.core.add.Add
+        A sympy representation of the operator, written in terms of Pauli's, e.g.
+        1.0*I0 + 0.5*X0 + 1.5*X1 - 0.5*I*Y0 - 0.5*I*Y1
+    """
+    qubit_indx0 = index_map[(matrix_idx, generator_idx)]
+    operator = 0
+    for j in range(number_states_per_oscillator-1):
+        bra_bitstring = f'{j:0{bits_per_oscillator}b}'
+        ket_bitstring = f'{j+1:0{bits_per_oscillator}b}'
+        for i in range(bits_per_oscillator):
+            new_term = single_qubit_state_map[bra_bitstring[i] + ket_bitstring[i]]
+            new_term = new_term.subs('I', f'I{qubit_indx0+i}')
+            new_term = new_term.subs('X', f'X{qubit_indx0+i}')
+            new_term = new_term.subs('Y', f'Y{qubit_indx0+i}')
+            new_term = new_term.subs('Z', f'Z{qubit_indx0+i}')
+            operator += new_term
+    return operator
+
+
+def creation_operator_old(matrix_idx: int, generator_idx: int) -> sp.core.add.Add:
+    """
+    Build the creation operator for the oscillator.
+
+    Parameters
+    ----------
+    matrix_idx : int
+        The SO(3) index enumerating the matrices (1, 2, 3)
+    generator_idx : int
+        The generator index (1, 2, ..., N^2)
+
+    Returns
+    -------
+    sp.core.add.Add
+        A sympy representation of the operator, written in terms of Pauli's, e.g.
+        1.0*I0 + 0.5*X0 + 1.5*X1 - 0.5*I*Y0 - 0.5*I*Y1
+    """
+    qubit_indx0 = index_map[(matrix_idx, generator_idx)]
+    operator = 0
+    for j in range(number_states_per_oscillator-1):
+        bra_bitstring = f'{j+1:0{bits_per_oscillator}b}'
+        ket_bitstring = f'{j:0{bits_per_oscillator}b}'
+        for i in range(bits_per_oscillator):
+            new_term = single_qubit_state_map[bra_bitstring[i] + ket_bitstring[i]]
+            new_term = new_term.subs('I', f'I{qubit_indx0+i}')
+            new_term = new_term.subs('X', f'X{qubit_indx0+i}')
+            new_term = new_term.subs('Y', f'Y{qubit_indx0+i}')
+            new_term = new_term.subs('Z', f'Z{qubit_indx0+i}')
+            operator += new_term
+    return operator
 
 
 class SpecialUnitaryGroup():
