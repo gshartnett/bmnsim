fixed sign issue with quadratic constraints

bmn/bootstrap.py
@@ -349,7 +349,7 @@ class BootstrapSystem:
             matrix_shape=(len(constraints), len(self.operator_list)),
         )
 
-    def build_quadratic_constraints(self) -> dict[str, np.ndarray]:
+    def build_quadratic_constraints(self, impose_linear_constraints=True) -> dict[str, np.ndarray]:
         """
         Build the quadratic constraints. The quadratic constraints are exclusively due to
         the cyclic constraints. The constraints can be written as
@@ -378,6 +378,7 @@ class BootstrapSystem:
         quadratic_terms = []
 
         # loop over constraints
+        counter = 0
         for constraint_idx, (operator_idx, term) in enumerate(constraints.items()):
 
             # check that either both LHS and RHS are trivial, or neither are
@@ -422,32 +423,47 @@ class BootstrapSystem:
                             quadratic_constraint_vector_2, quadratic_constraint_vector_1
                         )
 
-                # transform to null basis
-                linear_constraint_vector = np.dot(
-                    linear_constraint_vector, null_space_matrix
-                )
-                quadratic_matrix = np.einsum(
-                    "ia, ij, jb->ab",
-                    null_space_matrix,
-                    quadratic_matrix,
-                    null_space_matrix,
-                )
+                # TODO remove this
+                if impose_linear_constraints:
+                    # transform to null basis
+                    linear_constraint_vector = np.dot(
+                        linear_constraint_vector, null_space_matrix
+                    )
+                    quadratic_matrix = np.einsum(
+                        "ia, ij, jb->ab",
+                        null_space_matrix,
+                        quadratic_matrix,
+                        null_space_matrix,
+                    )
+                else:
+                    print("Note: not imposing linear constraints by transforming to Null basis.")
 
                 linear_is_zero = (np.max(np.abs(linear_constraint_vector)) < self.tol)
                 quadratic_is_zero = (np.max(np.abs(quadratic_matrix)) < self.tol)
 
                 if (not quadratic_is_zero) and (not linear_is_zero):
+                    #print(f"constraint {counter} corresponds to operator_idx {operator_idx}, op = {self.operator_list[operator_idx]}")
                     linear_terms.append(linear_constraint_vector)
                     quadratic_terms.append(quadratic_matrix)
+                    counter += 1
 
                 if quadratic_is_zero and not linear_is_zero:
                     print(f"constraint from operator_idx = {operator_idx} is quadratically trivial.")
                 elif not quadratic_is_zero and linear_is_zero:
                     print(f"constraint operator_idx = {operator_idx} is linearly trivial.")
 
+        # map to numpy arrays
+        # THE MINUS SIGN IS VERY IMPORTANT: (-RHS + LHS = 0)
+        quadratic_terms = -np.asarray(quadratic_terms)
+        linear_terms = np.asarray(linear_terms)
+
+        # check that the quadratic matrix is symmetric
+        if not np.allclose(quadratic_terms, np.einsum('Iab->Iba', quadratic_terms)):
+            raise ValueError("Quadratic matrix not symmetric.")
+
         return {
-            "linear": np.asarray(linear_terms),
-            "quadratic": np.asarray(quadratic_terms),
+            "linear": linear_terms,
+            "quadratic": quadratic_terms,
         }
 
     def clean_constraints(

bmn/solver.py
@@ -45,6 +45,7 @@ acknowledge https://github.com/gshartnett/matrix-bootstrap/blob/main/solver.py
 
 
 def debug(x):
+    # TODO remove this - hold over from Han et al
     print(x)
 
 
@@ -52,17 +53,26 @@ def compute_L2_norm_of_linear_constraints(A, b, param):
     return sum(np.square(A @ param - b))
 
 
-def compute_L2_norm_of_quadratic_constraints(bootstrap, param):
-    quadratic_constraints = bootstrap.build_quadratic_constraints()
-    constraints = quadratic_constraints['linear'] @ param + np.einsum('Iij, i, j -> I', quadratic_constraints['quadratic'], param, param)
+def compute_L2_norm_of_quadratic_constraints(quadratic_constraints, param):
+    # quadratic_constraints = bootstrap.build_quadratic_constraints()
+    constraints = quadratic_constraints["linear"] @ param + np.einsum(
+        "Iij, i, j -> I", quadratic_constraints["quadratic"], param, param
+    )
     return np.sum(np.square(constraints))
+
 
 def minimal_eigval(bootstrap_array_sparse, parameter_vector_null):
     dim = int(np.sqrt(bootstrap_array_sparse.shape[0]))
-    tables_val = np.reshape(
+    bootstrap_matrix = np.reshape(
         bootstrap_array_sparse.dot(parameter_vector_null), (dim, dim)
     )
-    return scipy.linalg.eigvalsh(tables_val)[0]
+
+    if not np.allclose(
+        (bootstrap_matrix - bootstrap_matrix.T), np.zeros_like(bootstrap_matrix)
+    ):
+        raise ValueError("Bootstrap matrix is not symmetric.")
+
+    return scipy.linalg.eigvalsh(bootstrap_matrix)[0]
 
 
 def sdp_init(
@@ -267,7 +277,7 @@ def get_quadratic_constraint_vector(
 def minimize(
     bootstrap,
     op,
-    init,
+    #init,
     op_cons=[SingleTraceOperator(data={(): 1})],
     maxiters=25,
     eps=5e-4,
@@ -297,6 +307,7 @@ def minimize(
     linear_constraint_matrix = bootstrap.build_linear_constraints().tocsr()
     quadratic_constraints = bootstrap.build_quadratic_constraints()
     bootstrap_array_sparse = bootstrap.build_bootstrap_table()
+    init = np.random.normal(size=bootstrap.param_dim_null)
 
     vec = bootstrap.single_trace_to_coefficient_vector(op, return_null_basis=True)
 
@@ -351,8 +362,12 @@ def minimize(
             eps=1e-4,
             verbose=True,
         )
-        linear_constraint_norm = compute_L2_norm_of_linear_constraints(A=A_op, b=b_op, param=param)
-        quadratic_constraint_norm = compute_L2_norm_of_quadratic_constraints(bootstrap=bootstrap, param=param)
+        linear_constraint_norm = compute_L2_norm_of_linear_constraints(
+            A=A_op, b=b_op, param=param
+        )
+        quadratic_constraint_norm = compute_L2_norm_of_quadratic_constraints(
+            quadratic_constraints=quadratic_constraints, param=param
+        )
         print(f"SDP INIT, ||A x - b||_2 = {linear_constraint_norm}")
         print(f"SDP INIT, ||quadratic constraint||_2 = {quadratic_constraint_norm}")
         radius = np.linalg.norm(param) + 20
@@ -381,8 +396,12 @@ def minimize(
             eps=1e-4,
             verbose=True,
         )
-        linear_constraint_norm = compute_L2_norm_of_linear_constraints(A=A_op, b=b_op, param=param)
-        quadratic_constraint_norm = compute_L2_norm_of_quadratic_constraints(bootstrap=bootstrap, param=param)
+        linear_constraint_norm = compute_L2_norm_of_linear_constraints(
+            A=A_op, b=b_op, param=param
+        )
+        quadratic_constraint_norm = compute_L2_norm_of_quadratic_constraints(
+            quadratic_constraints=quadratic_constraints, param=param
+        )
         print(f"SDP INIT, ||A x - b||_2 = {linear_constraint_norm}")
         print(f"SDP INIT, ||quadratic constraint||_2 = {quadratic_constraint_norm}")
         print("finish sdp_relax")